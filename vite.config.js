a7cd2cf4fb67b4da86cb7217a8ad8abdd7631b3d
Ally Osman <ally.a.ngonyany36@gmail.com>
2025-10-11 12:21:14 +0300

    Set correct base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/agent_ai",
+  base: "/agent_ai/",
 });
